9e5445a75a17f9abed950ff4bb4b9364d7cbdb1c
Andrey Sak <andreysak@MacBook-13--Andrey-Sak.local>
2024-10-17 19:44:50 +0300

    automatic framework update

---
+++ Package.swift
@@ -0,0 +1,65 @@
+// swift-tools-version:5.9
+
+import PackageDescription
+
+let faceARversionRange: Range<Version> = "1.14.0"..<"1.14.99"
+let package = Package(
+  name: "BanubaPhotoEditorSDK",
+  platforms: [
+    .iOS(.v15)
+  ],
+  products: [
+    .library(
+      name: "BanubaPhotoEditorSDK",
+      targets: ["BanubaPhotoEditorSDKTarget"]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/sdk-banuba/BNBSdkCore.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBSdkApi.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBEffectPlayer.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBScripting.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBLips.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBHair.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBEyes.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBBackground.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBSkin.git", faceARversionRange),
+    .package(url: "https://github.com/sdk-banuba/BNBAcneEyebagsRemoval.git", faceARversionRange),
+    .package(url: "https://github.com/Banuba/BanubaUtilities-iOS.git", from: "1.36.0"),
+    .package(url: "https://github.com/Banuba/BanubaLicenseServicingSDK.git", from: "1.36.0"),
+    .package(url: "https://github.com/Banuba/BNBLicenseUtils-iOS.git", from: "1.36.0")
+  ],
+  targets: [
+    .target(
+      name: "BanubaPhotoEditorSDKWrapper",
+      dependencies: [
+        "BanubaPhotoEditorSDK",
+        "BNBSdkCore",
+        "BNBSdkApi",
+        "BNBEffectPlayer",
+        "BNBScripting",
+        "BNBLips",
+        "BNBHair",
+        "BNBEyes",
+        "BNBBackground",
+        "BNBSkin",
+        "BNBAcneEyebagsRemoval",
+        "BanubaLicenseServicingSDK",
+        .product(name: "BanubaUtilities", package: "BanubaUtilities-iOS"),
+        .product(name: "BNBLicenseUtils", package: "BNBLicenseUtils-iOS")
+      ],
+      path: "BanubaPhotoEditorSDKWrapper"
+    ),
+    .target(
+      name: "BanubaPhotoEditorSDKTarget",
+      dependencies: [
+        .target(name: "BanubaPhotoEditorSDKWrapper")
+      ],
+      path: "BanubaPhotoEditorSDKTarget"
+    ),
+    .binaryTarget(
+      name: "BanubaPhotoEditorSDK",
+      path: "BanubaPhotoEditorSDK.xcframework"
+    )
+  ]
+)
