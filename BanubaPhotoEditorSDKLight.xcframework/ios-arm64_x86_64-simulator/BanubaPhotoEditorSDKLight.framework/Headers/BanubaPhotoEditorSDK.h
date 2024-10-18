e3d4c31bce29fb554529bad5f61a614d1cd1af69
Andrey Sak <andreysak@MacBook-13--Andrey-Sak.local>
2024-10-18 10:32:48 +0300

    automatic framework update

---
+++ BanubaPhotoEditorSDKLight.xcframework/ios-arm64_x86_64-simulator/BanubaPhotoEditorSDKLight.framework/Headers/BanubaPhotoEditorSDK.h
@@ -0,0 +1,20 @@
+//
+//  BanubaPhotoEditorSDK.h
+//  BanubaPhotoEditorSDK
+//
+//  Created by Rostyslav Dovhaliuk on 18.10.2023.
+//  Copyright © 2023 Banuba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+//! Project version number for BanubaPhotoEditorSDK.
+FOUNDATION_EXPORT double BanubaPhotoEditorSDKVersionNumber;
+
+//! Project version string for BanubaPhotoEditorSDK.
+FOUNDATION_EXPORT const unsigned char BanubaPhotoEditorSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BanubaPhotoEditorSDK/PublicHeader.h>
+
+
